504b9741d8f445688205986705c3bc9a19576e9b
Stephen Parsons <stephen.r.parsons@gmail.com>
2016-02-10 22:29:41 -0500

    working prototype, now just need alumni data

--- alumniMap.js
+++ alumniMap.js
@@ -16,67 +16,36 @@ function initMap() {
 	simpleSheet: true,
     });
 
-    var dataLength;
-    function processInfo(data, tabletop) {
-	dataLength = data.length;
-	for (var i = 0; i < dataLength; i++) {
-	    codeAddress(data[i], createMarker);
-	}
-    }
-
-    function codeAddress(data, fn) {
-	geocoder.geocode({'address': data.address}, function(results, status) {
-	    if (status == google.maps.GeocoderStatus.OK) {
-		fn(data, results[0].geometry.location);
-	    } else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
-		setTimeout(function() {
-		    codeAddress(data, fn);
-		}, 100);
-	    }
-	});
-    }
-
     var markers = [];
     var markerCluster;
-    function createMarker(data, location) {
-	var marker = new google.maps.Marker({'position': location, map: map});
-	markers.push(marker);
-	console.log(markers.length, dataLength);
-	if (markers.length == dataLength) {
-	    // markerCluster = new MarkerClusterer(map, markers);	    
+    var infoWindow;
+    function processInfo(data, tabletop) {
+	infoWindow = new google.maps.InfoWindow({maxWidth: 250});
+	for (var i = 0; i < data.length; i++) {
+	    var latLng = data[i].latLong.split(',');
+	    var position = {'lat': parseFloat(latLng[0]), 'lng': parseFloat(latLng[1])};
+	    var marker = new google.maps.Marker({'position': position, map: map});
+	    marker.addListener('click', (openInfoWindow(marker, data[i])));
+	    markers.push(marker);
 	}
+	// markerCluster = new MarkerClusterer(map, markers);
     }
-    
-    // this can contain html
-    var personInfo = {
-	name: 'Seattle Guy',
-	description: 'This guy lives in Seattle and works at a place. He used to work at another place. Before living in Seattle he lived somewhere else. He used to be a Gaines fellow.',
-	email: 'guyinseattle@gmail.com',
-    };
 
-    var contentString = '<p style="margin: 20px 0 20px 0"><h3>'+
-	personInfo.name +
-	'</h3></p>'+
-	'<p style="margin: 20px 0 20px 0">'+
-	personInfo.description +
-	'<p style="margin: 20px 0 20px 0">Email: <a href="mailto:'+
-	personInfo.email +
-	'">'+
-	personInfo.email +
-	'</a></p>';
+    function openInfoWindow(marker, personInfo) {
+	var contentString = '<p style="margin: 20px 0 20px 0"><h3>'+
+	    personInfo.name +
+	    '</h3></p>'+
+	    '<p style="margin: 20px 0 20px 0">'+
+	    personInfo.description +
+	    '<p style="margin: 20px 0 20px 0">Email: <a href="mailto:'+
+	    personInfo.email +
+	    '">'+
+	    personInfo.email +
+	    '</a></p>';
 
-    var infowindow = new google.maps.InfoWindow({
-	content: contentString,
-	maxWidth: 250,
-    });
-
-    var marker = new google.maps.Marker({
-	position: {lat: 47.6, lng: -122.3},
-	map: map,
-	title: 'Guy in Seattle',
-    });
-
-    marker.addListener('click', function() {
-	infowindow.open(map, marker);
-    });
+	return function() {
+	    infoWindow.setContent(contentString);
+	    infoWindow.open(map, marker);
+	};
+    }
 }
